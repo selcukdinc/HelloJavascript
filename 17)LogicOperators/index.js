d25c08938f562ca44852ad6a6c53694e372f9b4f
Selçuk DİNÇ <09.mesgul_dosya@icloud.com>
2024-08-31 02:36:54 +0300

    17. Video - Logic Operators

---
+++ 17)LogicOperators/index.js
@@ -0,0 +1,24 @@
+/* 
+    Logical operators = used to combine or manipulate boolean values
+                        (true or false)
+                        AND = &&
+                        OR  = ||
+                        NOT = !
+*/
+
+
+/* const temp = 250;
+
+if (temp <= 0 || temp > 30) {
+    console.log("The weather is BAD");
+}else{
+    console.log("The weather is GOOD");
+} */
+
+const isSunny = true;
+
+if (!isSunny) {
+    console.log("It is CLOUDY");
+}else{
+    console.log("It is SUNNY");
+}
