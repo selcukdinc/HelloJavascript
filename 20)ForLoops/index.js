aa9060e37c083b0909a5e8aa99c151988b2cc460
Selçuk DİNÇ <09.mesgul_dosya@icloud.com>
2024-09-04 01:43:23 +0300

    20. Video - For Loops

---
+++ 20)ForLoops/index.js
@@ -0,0 +1,18 @@
+// for loop = repeat some code a LIMITED amount of times
+/* 
+for(let i = 10; i > 0; i--){
+    console.log(`${i}!`);
+}
+console.log("HAPPY NEW YEAR!!"); 
+*/
+
+// Break statement
+for(let i = 1;i <= 20; i++){
+    if (i == 13) {
+        // continue;
+        break;
+    }else{
+        console.log(i);
+    }
+    
+}
